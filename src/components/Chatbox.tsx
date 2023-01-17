6a8c3786d2c6b805a9ada734b644e7bbd43f7888
Robert Teets <teetsrobert@gmail.com>
2023-01-17 13:19:26 -0700

    feat: added more styling for chatbox, working on improving layout.

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -4,7 +4,7 @@ import MessageForm from "./MessageForm";
 import LoginForm from "./LoginForm";
 
 interface IProps {
-    socket: Socket | undefined;
+    socket: Socket | any;
 }
 
 interface IMessage {
@@ -16,7 +16,7 @@ const Chatbox: FunctionComponent<IProps> = ({ socket }) => {
     const [messages, setMessages] = useState<Array<IMessage>>([]);
     const [connected, setConnected] = useState(false);
 
-    socket?.on("add_message", (message) => {
+    socket?.on("add_message", (message: IMessage) => {
         setMessages([...messages, message]);
     });
 
@@ -30,14 +30,27 @@ const Chatbox: FunctionComponent<IProps> = ({ socket }) => {
 
     return (
         <div className="bg-white w-2/3 shadow-lg h-[90vh] flex flex-col relative">
-            <div className="absolute top-[15%] left-0 right-0 h-[70%] p-5 flex flex-col gap-3">
+            {socket.auth && socket.auth.room && (
+                <div className="absolute top-0 left-0 right-0 h-[15%] bg-orange-400 flex items-center justify-center">
+                    <h2 className="text-4xl">
+                        You are connected to:{" "}
+                        <span className="font-bold">{socket.auth.room}</span>
+                    </h2>
+                </div>
+            )}
+            <div className="absolute top-[15%] left-0 right-0 max-h-[70%] p-5 flex flex-col gap-3 overflow-y-scroll">
                 {messages.map((message: IMessage, index) => {
                     return (
                         <div
                             className="bg-neutral-200 shadow-lg p-3"
                             key={index}
                         >
-                            <p>{message.author} said: </p>
+                            <p>
+                                <span className="font-bold">
+                                    {message.author}
+                                </span>{" "}
+                                said:{" "}
+                            </p>
                             <p className="ml-5">{message.content}</p>
                         </div>
                     );
